ba77fbaca83209777c30d2712763be89a3a538f0
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2018-05-27 16:41:54 -0300

    Melhora o calculo de possibilidades do peao

--- Chess/src/model/Pawn.java
+++ Chess/src/model/Pawn.java
@@ -5,14 +5,27 @@ import java.util.ArrayList;
 
 public class Pawn extends Piece {
 	
+	private boolean moved = false;
+
 	public Pawn(Color color) {
 		super(color);
 	}
 	
+	public void setMoved(boolean moved) {
+		this.moved = moved;
+	}
+
 	public Point[] movePossibilities(Board domain, Point from) {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
 		if(from.y < Board.dimension - 1) {
 			Point p = (Point)from.clone();
+			if(!moved) {
+				p.y = from.y + 2;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+			}
+
 			p.y = from.y + 1;
 			if(domain.squareIsVacant(p)) {
 				possibilitiesList.add((Point)p.clone());
@@ -26,7 +39,8 @@ public class Pawn extends Piece {
 				}
 			}
 			
-			p.x += 2;
+			p.x = from.x;
+			p.x += 1;
 			if(p.x < Board.dimension && !domain.squareIsVacant(p)) {
 				final Piece piece = domain.getPieceAt(p);
 				if(piece.getColor() != pieceColor) {
@@ -35,7 +49,7 @@ public class Pawn extends Piece {
 			}
 		}
 		
-		final Point[] possibilitiesArray = (Point[]) possibilitiesList.toArray();
+		final Point[] possibilitiesArray = possibilitiesList.toArray(new Point[possibilitiesList.size()]);
 		
 		return possibilitiesArray;
 	}
