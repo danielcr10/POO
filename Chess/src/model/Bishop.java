72ec2ede9b9d978132ad1946be877d9b4be87921
Daniel <daniel.ahnuc10@gmail.com>
2018-05-27 02:51:41 -0300

    Movimentação do bispo

--- Chess/src/model/Bishop.java
+++ Chess/src/model/Bishop.java
@@ -11,7 +11,76 @@ public class Bishop extends Piece {
 	
 	Point[] movePossibilities(Board domain, Point from) {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
-		
+		if(from.y < Board.dimension - 1) {
+			Point p = (Point)from.clone();
+			p.x = from.x;
+			p.y = from.y;
+			for(int i=1;p.y<=Board.dimension - 1 && p.x<=Board.dimension - 1;i++) {
+				p.x = p.x + i;
+				p.y = p.y + i;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != pieceColor) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+			}
+			
+			p.x = from.x;
+			p.y = from.y;
+			for(int k=1;p.y<=Board.dimension - 1 && p.x>=0;k++) {
+				p.x = p.x - k;
+				p.y = p.y + k;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != pieceColor) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+			}
+			
+			p.x = from.x;
+			p.y = from.y;
+			for(int j=1;p.y>=0 && p.x<=Board.dimension - 1;j++) {
+				p.x = p.x + j;
+				p.y = p.y - j;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != pieceColor) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+			}
+			
+			p.x = from.x;
+			p.y = from.y;
+			for(int l=1;p.y>=0 && p.x>=0;l++) {
+				p.x = p.x - l;
+				p.y = p.y - l;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != pieceColor) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+			}
+		}
 		final Point[] possibilitiesArray = (Point[]) possibilitiesList.toArray();
 		
 		return possibilitiesArray;
