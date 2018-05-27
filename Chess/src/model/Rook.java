18c91a0c90a344f2d0087ea653f24d1a526841a3
Daniel <daniel.ahnuc10@gmail.com>
2018-05-27 00:46:45 -0300

    Movimentação da torre

--- Chess/src/model/Rook.java
+++ Chess/src/model/Rook.java
@@ -11,7 +11,65 @@ public class Rook extends Piece {
 	
 	Point[] movePossibilities(Board domain, Point from) {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
-		
+		if(from.y < Board.dimension - 1) {
+			Point p = (Point)from.clone();
+			p.y = from.y;
+			for(int i=1;p.y<=Board.dimension - 1;i++) {
+				p.y = p.y + i;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != this.getColor()) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+		}
+			p.y = from.y;
+			for(int k=1;p.y>0;k--) {
+				p.y = p.y - k;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != this.getColor()) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+		}
+			p.x = from.x;
+			for(int j=1;p.x<=Board.dimension - 1;j++) {
+				p.x = p.x + j;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != this.getColor()) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+		}
+			p.x = from.x;
+			for(int l=1;p.x>0;l++) {
+				p.x = p.x + l;
+				if(domain.squareIsVacant(p)) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				else {
+					final Piece piece = domain.getPieceAt(p);
+					if(piece.getColor() != this.getColor()) {
+						possibilitiesList.add((Point)p.clone());
+					}
+					break;
+				}
+		}
+		}
 		final Point[] possibilitiesArray = (Point[]) possibilitiesList.toArray();
 		
 		return possibilitiesArray;	
