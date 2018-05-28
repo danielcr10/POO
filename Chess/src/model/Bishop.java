f3b61a452dec436ac423f52d1782c7a1a2b5780f
Daniel <daniel.ahnuc10@gmail.com>
2018-05-27 21:39:37 -0300

    Ajustes de movimento do bispo

--- Chess/src/model/Bishop.java
+++ Chess/src/model/Bishop.java
@@ -13,9 +13,7 @@ public class Bishop extends Piece {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
 		if(from.y < Board.dimension - 1) {
 			Point p = (Point)from.clone();
-			p.x = from.x;
-			p.y = from.y;
-			for(int i=1;p.y<=Board.dimension - 1 && p.x<=Board.dimension - 1;i++) {
+			for(int i=1;p.y<Board.dimension - 1 && p.x<Board.dimension - 1;i++) {
 				p.x = from.x + i;
 				p.y = from.y + i;
 				if(domain.squareIsVacant(p)) {
@@ -30,9 +28,7 @@ public class Bishop extends Piece {
 				}
 			}
 			
-			p.x = from.x;
-			p.y = from.y;
-			for(int k=1;p.y<=Board.dimension - 1 && p.x>=0;k++) {
+			for(int k=1;p.y<Board.dimension - 1 && p.x>=0;k++) {
 				p.x = from.x - k;
 				p.y = from.y + k;
 				if(domain.squareIsVacant(p)) {
@@ -47,9 +43,7 @@ public class Bishop extends Piece {
 				}
 			}
 			
-			p.x = from.x;
-			p.y = from.y;
-			for(int j=1;p.y>=0 && p.x<=Board.dimension - 1;j++) {
+			for(int j=1;p.y>=0 && p.x<Board.dimension - 1;j++) {
 				p.x = from.x + j;
 				p.y = from.y - j;
 				if(domain.squareIsVacant(p)) {
@@ -64,8 +58,6 @@ public class Bishop extends Piece {
 				}
 			}
 			
-			p.x = from.x;
-			p.y = from.y;
 			for(int l=1;p.y>=0 && p.x>=0;l++) {
 				p.x = from.x - l;
 				p.y = from.y - l;
