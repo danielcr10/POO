da7ba5355fe38ced3c41714ed2e4dfb410ea8edc
Daniel <daniel.ahnuc10@gmail.com>
2018-05-27 21:03:50 -0300

    Ajustes na condição de movimentação da torre e ininicialização de variáveis

--- Chess/src/model/Rook.java
+++ Chess/src/model/Rook.java
@@ -10,64 +10,58 @@ public class Rook extends Piece {
 	}
 	
 	Point[] movePossibilities(Board domain, Point from) {
-		final ArrayList<Point> possibilitiesList = new ArrayList<>();
-		if(from.y < Board.dimension - 1) {
-			Point p = (Point)from.clone();
-			p.y = from.y;
-			for(int i=1;p.y<=Board.dimension - 1;i++) {
-				p.y = from.y + i;
-				if(domain.squareIsVacant(p)) {
-					possibilitiesList.add((Point)p.clone());
-				}
-				else {
-					final Piece piece = domain.getPieceAt(p);
-					if(piece.getColor() != pieceColor) {
-						possibilitiesList.add((Point)p.clone());
-					}
-					break;
-				}
+		final ArrayList<Point> possibilitiesList = new ArrayList<>(); 
+		Point p = (Point)from.clone();
+		for(int i=1;p.y<Board.dimension - 1;i++) {
+			p.y = from.y + i;
+			if(domain.squareIsVacant(p)) {
+				possibilitiesList.add((Point)p.clone());
 			}
-			p.y = from.y;
-			for(int k=1;p.y>0;k--) {
-				p.y = from.y - k;
-				if(domain.squareIsVacant(p)) {
+			else {
+				final Piece piece = domain.getPieceAt(p);
+				if(piece.getColor() != pieceColor) {
 					possibilitiesList.add((Point)p.clone());
 				}
-				else {
-					final Piece piece = domain.getPieceAt(p);
-					if(piece.getColor() != pieceColor) {
-						possibilitiesList.add((Point)p.clone());
-					}
-					break;
-				}
+				break;
 			}
-			p.x = from.x;
-			for(int j=1;p.x<=Board.dimension - 1;j++) {
-				p.x = from.x + j;
-				if(domain.squareIsVacant(p)) {
-					possibilitiesList.add((Point)p.clone());
-				}
-				else {
-					final Piece piece = domain.getPieceAt(p);
-					if(piece.getColor() != pieceColor) {
-						possibilitiesList.add((Point)p.clone());
-					}
-					break;
-				}
+		}
+		for(int k=1;p.y>0;k--) {
+			p.y = from.y - k;
+			if(domain.squareIsVacant(p)) {
+				possibilitiesList.add((Point)p.clone());
 			}
-			p.x = from.x;
-			for(int l=1;p.x>0;l++) {
-				p.x = from.x + l;
-				if(domain.squareIsVacant(p)) {
+			else {
+				final Piece piece = domain.getPieceAt(p);
+				if(piece.getColor() != pieceColor) {
 					possibilitiesList.add((Point)p.clone());
 				}
-				else {
-					final Piece piece = domain.getPieceAt(p);
-					if(piece.getColor() != pieceColor) {
-						possibilitiesList.add((Point)p.clone());
-					}
-					break;
+				break;
+			}
+		}
+		for(int j=1;p.x<Board.dimension - 1;j++) {
+			p.x = from.x + j;
+			if(domain.squareIsVacant(p)) {
+				possibilitiesList.add((Point)p.clone());
+			}
+			else {
+				final Piece piece = domain.getPieceAt(p);
+				if(piece.getColor() != pieceColor) {
+					possibilitiesList.add((Point)p.clone());
 				}
+				break;
+			}
+		}
+		for(int l=1;p.x>0;l++) {
+			p.x = from.x + l;
+			if(domain.squareIsVacant(p)) {
+				possibilitiesList.add((Point)p.clone());
+			}
+			else {
+				final Piece piece = domain.getPieceAt(p);
+				if(piece.getColor() != pieceColor) {
+					possibilitiesList.add((Point)p.clone());
+				}
+				break;
 			}
 		}
 		final Point[] possibilitiesArray = (Point[]) possibilitiesList.toArray();
