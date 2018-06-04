94279c6e3811fd80e66b6c40609029bac3d3cef8
Daniel Cunha Rios <daniel.ahnuc10@gmail.com>
2018-06-04 13:54:40 -0300

    Ajustes movimentação

--- Chess/src/model/Queen.java
+++ Chess/src/model/Queen.java
@@ -12,6 +12,7 @@ public class Queen extends Piece {
 	public Point[] movePossibilities(Board domain, Point from) {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
 		Point p = (Point) from.clone();
+		p.y = from.y;
 		for (int i = 1; p.y < Board.dimension - 1; i++) {
 			p.y = from.y + i;
 			if (domain.squareIsVacant(p)) {
@@ -24,6 +25,7 @@ public class Queen extends Piece {
 				break;
 			}
 		}
+		p.y = from.y;
 		for (int k = 1; p.y > 0; k--) {
 			p.y = from.y - k;
 			if (domain.squareIsVacant(p)) {
@@ -36,7 +38,7 @@ public class Queen extends Piece {
 				break;
 			}
 		}
-		p.y = from.y;
+		p.x = from.x;
 		for (int j = 1; p.x < Board.dimension - 1; j++) {
 			p.x = from.x + j;
 			if (domain.squareIsVacant(p)) {
@@ -49,6 +51,7 @@ public class Queen extends Piece {
 				break;
 			}
 		}
+		p.x = from.x;
 		for (int l = 1; p.x > 0; l++) {
 			p.x = from.x - l;
 			if (domain.squareIsVacant(p)) {
@@ -61,6 +64,8 @@ public class Queen extends Piece {
 				break;
 			}
 		}
+		p.x = from.x;
+		p.y = from.y;
 		for (int ii = 1; p.y < Board.dimension - 1 && p.x < Board.dimension - 1; ii++) {
 			p.x = from.x + ii;
 			p.y = from.y + ii;
@@ -74,6 +79,8 @@ public class Queen extends Piece {
 				break;
 			}
 		}
+		p.x = from.x;
+		p.y = from.y;
 		for (int kk = 1; p.y < Board.dimension - 1 && p.x > 0; kk++) {
 			p.x = from.x - kk;
 			p.y = from.y + kk;
@@ -87,6 +94,8 @@ public class Queen extends Piece {
 				break;
 			}
 		}
+		p.x = from.x;
+		p.y = from.y;
 		for (int jj = 1; p.y > 0 && p.x < Board.dimension - 1; jj++) {
 			p.x = from.x + jj;
 			p.y = from.y - jj;
@@ -100,6 +109,8 @@ public class Queen extends Piece {
 				break;
 			}
 		}
+		p.x = from.x;
+		p.y = from.y;
 		for (int ll = 1; p.y > 0 && p.x > 0; ll++) {
 			p.x = from.x - ll;
 			p.y = from.y - ll;
