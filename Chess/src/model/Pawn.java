76cf4b898f243d9329715894703860aab171d611
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2018-06-04 02:30:45 -0300

    Conserta o movimento do peao considerando o sentido das jogadas

--- Chess/src/model/Pawn.java
+++ Chess/src/model/Pawn.java
@@ -17,35 +17,34 @@ public class Pawn extends Piece {
 
 	public Point[] movePossibilities(Board domain, Point from) {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
-		if(from.y < Board.dimension - 1) {
-			Point p = (Point)from.clone();
-			if(!moved) {
-				p.y = from.y + 2;
-				if(domain.squareIsVacant(p)) {
-					possibilitiesList.add((Point)p.clone());
-				}
-			}
-
-			p.y = from.y + 1;
+		final int sense = pieceColor.getValue();
+		Point p = (Point)from.clone();
+		if(!moved) {
+			p.y = from.y + 2 * sense;
 			if(domain.squareIsVacant(p)) {
 				possibilitiesList.add((Point)p.clone());
 			}
-			
-			p.x -= 1;
-			if(p.x >= 0 && !domain.squareIsVacant(p)) {
-				final Piece piece = domain.getPieceAt(p);
-				if(piece.getColor() != pieceColor) {
-					possibilitiesList.add((Point)p.clone());
-				}
+		}
+
+		p.y = from.y + 1 * sense;
+		if(domain.squareIsVacant(p)) {
+			possibilitiesList.add((Point)p.clone());
+		}
+
+		p.x -= 1;
+		if(p.x >= 0 && !domain.squareIsVacant(p)) {
+			final Piece piece = domain.getPieceAt(p);
+			if(piece.getColor() != pieceColor) {
+				possibilitiesList.add((Point)p.clone());
 			}
-			
-			p.x = from.x;
-			p.x += 1;
-			if(p.x < Board.dimension && !domain.squareIsVacant(p)) {
-				final Piece piece = domain.getPieceAt(p);
-				if(piece.getColor() != pieceColor) {
-					possibilitiesList.add((Point)p.clone());
-				}
+		}
+
+		p.x = from.x;
+		p.x += 1;
+		if(p.x < Board.dimension && !domain.squareIsVacant(p)) {
+			final Piece piece = domain.getPieceAt(p);
+			if(piece.getColor() != pieceColor) {
+				possibilitiesList.add((Point)p.clone());
 			}
 		}
 		
