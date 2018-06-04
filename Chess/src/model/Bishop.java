5349bf9c556fc5398610f45a7396e703a8312704
Daniel Cunha Rios <daniel.ahnuc10@gmail.com>
2018-06-04 13:47:41 -0300

    Ajuste movimento do bispo

--- Chess/src/model/Bishop.java
+++ Chess/src/model/Bishop.java
@@ -12,6 +12,9 @@ public class Bishop extends Piece {
 	public Point[] movePossibilities(Board domain, Point from) {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
 		Point p = (Point) from.clone();
+		//movimenta na diagonal direita inferior
+		p.x = from.x;
+		p.y = from.y;
 		for (int i = 1; p.y < Board.dimension - 1 && p.x < Board.dimension - 1; i++) {
 			p.x = from.x + i;
 			p.y = from.y + i;
@@ -25,7 +28,9 @@ public class Bishop extends Piece {
 				break;
 			}
 		}
-
+		//movimenta na diagonal esquerda inferior
+		p.x = from.x;
+		p.y = from.y;
 		for (int k = 1; p.y < Board.dimension - 1 && p.x > 0; k++) {
 			p.x = from.x - k;
 			p.y = from.y + k;
@@ -39,7 +44,9 @@ public class Bishop extends Piece {
 				break;
 			}
 		}
-
+		//movimenta na diagonal direita superior
+		p.x = from.x;
+		p.y = from.y;
 		for (int j = 1; p.y > 0 && p.x < Board.dimension - 1; j++) {
 			p.x = from.x + j;
 			p.y = from.y - j;
@@ -53,7 +60,9 @@ public class Bishop extends Piece {
 				break;
 			}
 		}
-
+		//movimenta na diagonal esquerda superior
+		p.x = from.x;
+		p.y = from.y;
 		for (int l = 1; p.y > 0 && p.x > 0; l++) {
 			p.x = from.x - l;
 			p.y = from.y - l;
