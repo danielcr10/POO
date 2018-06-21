831b7410e90d65476ec6e978472a469b5fef88da
Daniel <daniel.ahnuc10@gmail.com>
2018-06-21 19:39:40 -0300

    Ajuste do roque, quando a tore já se movimentou

--- Chess/src/model/King.java
+++ Chess/src/model/King.java
@@ -4,9 +4,9 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 class King extends Piece {
-	
+
 	private boolean moved = false;
-	
+
 	public void setMoved(boolean moved) {
 		this.moved = moved;
 	}
@@ -14,11 +14,11 @@ class King extends Piece {
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	public ArrayList<Point> movePossibilities() {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
-		for(Point p : getTargetPositions()) {
-			if(pieceBoard.contains(p) && pieceBoard.squareIsVacant(p) && !pieceBoard.pieceIsVulnerableAt(this, p)) {
+		for (Point p : getTargetPositions()) {
+			if (pieceBoard.contains(p) && pieceBoard.squareIsVacant(p) && !pieceBoard.pieceIsVulnerableAt(this, p)) {
 				possibilitiesList.add(p);
 			}
 		}
@@ -31,10 +31,11 @@ class King extends Piece {
 
 	public ArrayList<Point> attackPossibilities() {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
-		for(Point p : getTargetPositions()) {
-			if(pieceBoard.contains(p) && !pieceBoard.squareIsVacant(p)) {
+		for (Point p : getTargetPositions()) {
+			if (pieceBoard.contains(p) && !pieceBoard.squareIsVacant(p)) {
 				final Piece piece = pieceBoard.getPieceAt(p);
-				if(piece.getColor() != pieceColor && (piece instanceof King || !pieceBoard.pieceIsVulnerableAt(this, p))) {
+				if (piece.getColor() != pieceColor
+						&& (piece instanceof King || !pieceBoard.pieceIsVulnerableAt(this, p))) {
 					possibilitiesList.add(p);
 				}
 			}
@@ -58,55 +59,56 @@ class King extends Piece {
 
 		return targetPositions;
 	}
-	
+
 	private ArrayList<Point> castlePossibilities() {
 		final ArrayList<Point> possibilitiesList = new ArrayList<>();
 
-		if(!moved) {
+		if (!moved) {
 			final PieceSet set = pieceBoard.getPieceSet(pieceColor);
 			final ArrayList<Rook> rooks = set.getRooks();
-			Point p = (Point)this.getCurrentPosition().clone();
-			for(Rook rook : rooks) {
-				if(rook.getCurrentPosition().x == 0) {
-					p.x = 2;
-				}
-				else {
-					p.x = 6;
-				}
-				if(isVacantBtw(rook, this) && !pieceBoard.pieceIsVulnerableAt(this, p)) { //
-					possibilitiesList.add((Point)p.clone());
+			Point p = (Point) this.getCurrentPosition().clone();
+			for (Rook rook : rooks) {
+				if (!rook.getMoved()) {
+					if (rook.getCurrentPosition().x == 0) {
+						p.x = 2;
+					} else {
+						p.x = 6;
+					}
+					if (isVacantBtw(rook, this) && !pieceBoard.pieceIsVulnerableAt(this, p)) { //
+						possibilitiesList.add((Point) p.clone());
+					}
 				}
 			}
 		}
 
 		return possibilitiesList;
 	}
-	
+
 	private boolean isVacantBtw(Piece p1, Piece p2) {
 		boolean vacant = true;
 		Point aux = (Point) p2.getCurrentPosition().clone();
-		if(p1.getCurrentPosition().x < p2.getCurrentPosition().x) {
+		if (p1.getCurrentPosition().x < p2.getCurrentPosition().x) {
 			aux = (Point) p1.getCurrentPosition().clone();
 		}
-		for(int i = 1; i < Math.abs(p1.getCurrentPosition().x - p2.getCurrentPosition().x); i++) {
+		for (int i = 1; i < Math.abs(p1.getCurrentPosition().x - p2.getCurrentPosition().x); i++) {
 			aux.x++;
 			vacant = pieceBoard.squareIsVacant(aux);
-			if(!vacant) {
+			if (!vacant) {
 				return vacant;
 			}
 		}
 		return vacant;
 	}
-	
+
 	public void move(Point to) {
-		if(Math.abs(getCurrentPosition().x - to.x) > 1) {
+		if (Math.abs(getCurrentPosition().x - to.x) > 1) {
 			final Rook closestRook = getClosestRook(to);
 			closestRook.doCastle();
 		}
 		super.move(to);
 		moved = true;
 	}
-	
+
 	private Rook getClosestRook(Point to) {
 		Rook closestRook;
 		final PieceSet set = pieceBoard.getPieceSet(pieceColor);
@@ -114,10 +116,9 @@ class King extends Piece {
 		final Rook rook1 = rooks.get(0);
 		final Rook rook2 = rooks.get(1);
 
-		if(Math.abs((rook1.getCurrentPosition().x - to.x)) < Math.abs((rook2.getCurrentPosition().x - to.x))) {
+		if (Math.abs((rook1.getCurrentPosition().x - to.x)) < Math.abs((rook2.getCurrentPosition().x - to.x))) {
 			closestRook = rook1;
-		}
-		else {
+		} else {
 			closestRook = rook2;
 		}
 
